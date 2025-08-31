a2aacf126ba27e843348088f5d5c3dc878af7424
Kari Meghana <karimeghana02@gmail.com>
2025-08-31 15:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
-};
+	const prices = document.querySelectorAll('.prices');
+    let totalPrice = 0;
 
+    prices.forEach(price => {
+        totalPrice += parseFloat(price.textContent);
+    });
+
+    alert(`Total Price: ${totalPrice}`);
+};
 getSumBtn.addEventListener("click", getSum);
 
